ac1c8ebb20faafc1c54242e42dfdede358832505
斟酌 鵬兄 <tgckpg@gmail.com>
2021-03-26 08:41:27 +0800

    Initial commit

---
+++ LeaderElectionRun/KServices/KLog.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderElectionRun.KServices
+{
+	class KLog
+	{
+		private static KLog _Inst;
+		public static KLog Instance => _Inst ??= new KLog();
+
+		private readonly LoggerFactory _LogFactory;
+
+		public KLog()
+		{
+			_LogFactory = new LoggerFactory();
+			_LogFactory.AddProvider( new KLogProvider() );
+		}
+
+		public static ILogger GetLogger<T>()
+			=> Instance._LogFactory.CreateLogger<T>();
+	}
+
+	class KLogger : ILogger
+	{
+		public Dictionary<LogLevel, string> LogLevelMap = new()
+		{
+			{ LogLevel.Information, "INFO" },
+			{ LogLevel.Error, "ERROR" },
+			{ LogLevel.Warning, "WARN" },
+		};
+
+		public IDisposable BeginScope<TState>( TState state ) => default;
+
+		public bool IsEnabled( LogLevel logLevel )
+			=> true;
+
+		public void Log<TState>( LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter )
+			=> Console.WriteLine( $"[{DateTime.Now:dd-MM-yyyy HH:mm:ss}][{LogLevelMap[ logLevel ]}] {formatter( state, exception )}" );
+	}
+
+	class KLogProvider : ILoggerProvider
+	{
+		private readonly ConcurrentDictionary<string, KLogger> _Loggers = new();
+
+		public ILogger CreateLogger( string Cat )
+			=> _Loggers.GetOrAdd( Cat, new KLogger() );
+
+		public void Dispose()
+			=> _Loggers.Clear();
+	}
+}
---
+++ LeaderElectionRun/KServices/KStart.cs
@@ -0,0 +1,120 @@
+﻿using k8s;
+using k8s.LeaderElection;
+using k8s.LeaderElection.ResourceLock;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace LeaderElectionRun.KServices
+{
+    class KStart
+    {
+		public readonly string Namespace;
+        public string Identity;
+        public string LockName;
+
+		private CancellationTokenSource CTS;
+		private readonly LeaderElector Elector;
+
+		private readonly ILogger Logger;
+
+		public KStart( string Namespace, string LockName, string Identity )
+		{
+			Logger = KLog.GetLogger<KStart>();
+
+			this.Namespace = Namespace;
+			this.Identity = Identity;
+			this.LockName = LockName;
+
+			Kubernetes Kube = new( KubernetesClientConfiguration.BuildDefaultConfig() );
+			EndpointsLock EndPointLock = new( Kube, Namespace, LockName, Identity );
+			Elector = new( new LeaderElectionConfig( EndPointLock )
+			{
+				LeaseDuration = TimeSpan.FromSeconds( 1 ),
+				RetryPeriod = TimeSpan.FromMilliseconds( 400 )
+			} );
+
+			Elector.OnNewLeader += Elector_OnNewLeader;
+			Elector.OnStartedLeading += Elector_OnStartedLeading;
+			Elector.OnStoppedLeading += Elector_OnStoppedLeading;
+		}
+
+		public void Start()
+		{
+			while ( true )
+			{
+				CTS = new();
+				_ = Elector.RunAsync( CTS.Token );
+				Logger.LogInformation( "Started" );
+				Console.ReadKey( true );
+				CTS.Cancel();
+				CTS.Dispose();
+				Logger.LogInformation( "Stopped" );
+				Console.ReadKey( true );
+			}
+		}
+
+		public void Start( string PIdFile )
+		{
+			while ( true )
+			{
+				int PId;
+				try
+				{
+					using FileStream S = File.OpenRead( PIdFile );
+					using StreamReader S2 = new( S );
+					PId = int.Parse( S2.ReadLine() );
+				}
+				catch( AccessViolationException )
+				{
+					Logger.LogError( $"Access denied while reading: {PIdFile}" );
+					Thread.Sleep( 2000 );
+					continue;
+				}
+				catch( FileNotFoundException )
+				{
+					Logger.LogWarning( $"Waiting for file: {PIdFile}" );
+					Thread.Sleep( 2000 );
+					continue;
+				}
+
+				Process P;
+				try
+				{
+					P = Process.GetProcessById( PId );
+				}
+				catch ( ArgumentException )
+				{
+					Logger.LogWarning( $"No such process({PId})" );
+					Thread.Sleep( 2000 );
+					continue;
+				}
+
+				Logger.LogInformation( $"Monitoring: {P.ProcessName}, PID: {PId}" );
+				CTS = new();
+				_ = Elector.RunAsync( CTS.Token );
+				P.WaitForExit();
+				CTS.Cancel();
+				CTS.Dispose();
+				Logger.LogInformation( "Process has exited" );
+			}
+		}
+
+		private void Elector_OnStartedLeading()
+		{
+			Logger.LogInformation( $"Started Leading: {Identity}" );
+		}
+
+		private void Elector_OnStoppedLeading()
+		{
+			Logger.LogInformation( $"Stopped Leading: {Identity}" );
+		}
+
+		private void Elector_OnNewLeader( string obj )
+		{
+			Logger.LogInformation( $"New Leader: {Identity}" );
+		}
+	}
+}
